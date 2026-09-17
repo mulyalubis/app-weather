152d4f5b77388ebba07a906ae44cd1bb05079fdc
mulya lubis <mulyalubis2004@gmail.com>
2026-09-17 08:50:51 +0700

    memperbaiki crud pelilihan cuaca kota

--- lib/database/db_helper.dart
+++ lib/database/db_helper.dart
@@ -1,11 +1,40 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class DBHelper {
   static Database? _database;
 
   static Future<Database> get database async {
     if (_database != null) return _database!;
+    _database = await _initDB();
     return _database!;
+  }
+
+  static Future<Database> _initDB() async {
+    String path = join(await getDatabasesPath(), 'user_login.db');
+
+    return openDatabase(
+      path,
+      version: 2,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            is_login INTEGER DEFAULT 0
+          )
+        ''');
+
+        await db.execute('''
+        CREATE TABLE favorite_city (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          user_id INTEGER,
+          city_name TEXT
+        )
+      ''');
+      },
+    );
   }
 
   static Future<void> addFavoriteCity(String city) async {
